a4530dd6b3981fe2d6335aa2f262400d99d7280c
sms <secrue1u@gmail.com>
2018-05-02 11:55:01 +0900

    shift

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+ * led_onoff.cpp
+ *
+ * Created: 2018-05-02 오전 10:51:53
+ * Author : win_mini
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRD = 0xFF;
+	PORTD = 0xFF;
+	
+	int i;
+    /* Replace with your application code */
+    while (1) 
+    {
+		for(i = 0; i > 7; i++)
+		{
+			PORTD = ~(1<< i);
+			_delay_ms(500);
+			
+		}
+		for(i = 0; i > 7; i++)
+		{
+			PORTD = ~(0x80 >> i);
+			_delay_ms(500);
+		}
+		
+	
+    }
+}
+
